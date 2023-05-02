0363877a2a3cb7ecbd2b00b742b389c27e8daf74
absiddik676 <absiddik8200@gmail.com>
2023-05-02 21:13:20 +0600

    chefs data added and sent clint site

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const cors = require('cors')
+
+app.use(cors())
+
+
+const chefsData = require('./data/chefsDat.json')
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/chefs',(req,res) => {
+  res.send(chefsData)
 })
 
 app.listen(port, () => {
